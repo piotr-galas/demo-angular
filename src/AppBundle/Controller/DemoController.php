37c9e0e34eb1cbfb7224b1fae23a75c7e2de70fb
Piotr <galaspiotrek@gmail.com>
2015-06-22 11:31:00 +0200

    add 4 more action and rrelated view

--- src/AppBundle/Controller/DemoController.php
+++ src/AppBundle/Controller/DemoController.php
@@ -6,34 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DemoController extends Controller
 {
-//	public function indexAction()
-//	{
-//
-//	}
+	public function indexAction()
+	{
+		return $this->render('AppBundle:Demo:index.html.twig');
+	}
 
 	public function firstAction()
 	{
 		return $this->render('AppBundle:Demo:first.html.twig');
 	}
 
-//	public function secondAction()
-//	{
-//		return $this->render('default/index.html.twig');
-//	}
-//
-//	public function thirdAction()
-//	{
-//		return $this->render('default/index.html.twig');
-//	}
-//
-//	public function fourthAction()
-//	{
-//		return $this->render('default/index.html.twig');
-//	}
-//
-//	public function fifthAction()
-//	{
-//		return $this->render('default/index.html.twig');
-//	}
+	public function secondAction()
+	{
+		return $this->render('AppBundle:Demo:second.html.twig');
+	}
+
+	public function thirdAction()
+	{
+		return $this->render('AppBundle:Demo:third.html.twig');
+	}
+
+	public function fourthAction()
+	{
+		return $this->render('AppBundle:Demo:fourth.html.twig');
+	}
+
+	public function fifthAction()
+	{
+		return $this->render('AppBundle:Demo:fifth.html.twig');
+	}
 
 }
